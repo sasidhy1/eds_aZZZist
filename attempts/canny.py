80aec6a918caacb2ae06c74de8623509e684a40a
sasidhy1 <sasidhy1@gmail.com>
2018-02-28 01:43:11 -0500

    Modify for canny edge detection for realtime video stream

--- attempts/canny.py
+++ attempts/canny.py
@@ -2,12 +2,20 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('img.jpg',0)
-edges = cv2.Canny(img,100,200)
+cap = cv2.VideoCapture(0)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-plt.show()
+    # Our operations on the frame come here
+    canny = cv2.Canny(frame,100,200)
+
+    # Display the resulting frame
+    cv2.imshow('frame',canny)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
