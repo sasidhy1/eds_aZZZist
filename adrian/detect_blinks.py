7d0018ed173ee495d58caaea8aa6020d0f314de6
Yamini Sasidhar <sasidhy1@gmail.com>
2018-03-19 15:32:12 -0400

    Ensure all packages are available and up-to-date

---
+++ adrian/detect_blinks.py
@@ -0,0 +1,11 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
