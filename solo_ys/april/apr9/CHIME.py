ed9b17ee2f5a6681317d59fffd817514de062225
Yamini Sasidhar <sasidhy1@gmail.com>
2018-04-09 17:06:12 -0400

    Modify chime tones, ON chime and OFF chime

--- solo_ys/april/apr9/CHIME.py
+++ solo_ys/april/apr9/CHIME.py
@@ -3,16 +3,16 @@ import time
 
 ### ON CHIME ###
 
-d1 = 0.1  # second
+d1 = 0.2  # second
 f1 = 450  # Hz
-d2 = 0.1  # second
+d2 = 0.2  # second
 f2 = 550  # Hz
-d3 = 0.1  # second
+d3 = 0.2  # second
 f3 = 650  # Hz
 
-os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (d1, f1))
-os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (d2, f2))
-os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (d3, f3))
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f fade q .02 .05 .02' % (d1, f1))
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f fade q .02 .05 .02' % (d2, f2))
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f fade q .02 .05 .02' % (d3, f3))
 
 time.sleep(1)
 
@@ -23,3 +23,6 @@ f4 = 450  # Hz
 
 os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (d4, f4))
 os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (d4, f4))
+
+# os.system('play --no-show-progress --null --channels 1 synth %s sine %f fade q .02 .05 .02' % (d4, f4))
+# os.system('play --no-show-progress --null --channels 1 synth %s sine %f fade q .02 .05 .02' % (d4, f4))
