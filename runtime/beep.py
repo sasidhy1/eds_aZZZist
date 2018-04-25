3b32450ca26f5c8cc4d737220dfba7512105f67f
Yamini Sasidhar <sasidhy1@gmail.com>
2018-04-25 01:11:57 -0400

    Added files for runtime integration, executables

---
+++ runtime/beep.py
@@ -0,0 +1,9 @@
+import os
+from random import *
+import sys
+
+duration = 5  # second
+freq = 450  # Hz
+
+print("user why u sleppin")
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
